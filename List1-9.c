52e8657eade8f9b6bfb337cf91469aa23460ac66
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2023-06-30 15:22:27 +0800

    List01

---
+++ List1-9.c
@@ -0,0 +1,14 @@
+/*显示数组元素的个数和各元素的值*/
+#include<stdio.h>
+int main(void){
+    int i;
+    int a[]={1,2,3,4,5};
+    int na=sizeof(a)/sizeof(a[0]);
+
+    printf("数组a的元素个数是%d。\n",na);
+
+    for(i=0;i<na;i++)
+    printf("a[%d]= %d\n",i,a[i]);
+
+    return 0;
+}
